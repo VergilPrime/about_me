e15059200230446c137fcf05cec16ed2f3246e7b
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-09-19 15:38:10 -0700

    Updating all files

--- app.js
+++ app.js
@@ -4,24 +4,90 @@ var userName = prompt('Who goes there!?');
 
 alert('Hello ' + userName + ', see if you can pass my test.');
 
-var devNameGuess = prompt('Who do you think I am?').toLowerCase();
-	//keeping track of the correctness of the answers as booleans
-var boolAnswer1
-	
 	//QUESTION 1
+var devNameGuess = prompt('Who do you think I am?').toLowerCase();
+
 if(devNameGuess === 'dustin' || devNameGuess === 'dustin mundy' ){
-	//CORRECT!
+		//CORRECT!
 	alert('Correct! My name is Dustin! You may pass!')
-	boolAnswer1 = true
 	
 }else if(devNameGuess === 'vergil' || devNameGuess === 'vergilprime' ){
-	//CORRECT!
+		//CORRECT!
 	alert('Okay, that\'s my online moniker. You may pass.')
-	boolAnswer1 = true
 	
 }else{
-	//INCORRECT!
-	alert('Nope. You have failed.')
-	boolAnswer1 = false
+		//INCORRECT!
+	devNameGuess = prompt('Nope. Guess again.')
+	
+	if(devNameGuess === 'dustin' || devNameGuess === 'dustin mundy' ){
+			//CORRECT!
+		alert('Correct! My name is Dustin! You may pass!')
+		
+	}else if(devNameGuess === 'vergil' || devNameGuess === 'vergilprime' ){
+			//CORRECT!
+		alert('Okay, that\'s my online moniker. You may pass.')
+		
+	}else{
+			//INCORRECT!
+		devNameGuess = prompt('Nope. One more try?')
+		
+		if(devNameGuess === 'dustin' || devNameGuess === 'dustin mundy' ){
+				//CORRECT!
+			alert('Correct! My name is Dustin! You may pass!')
+			
+		}else if(devNameGuess === 'vergil' || devNameGuess === 'vergilprime' ){
+				//CORRECT!
+			alert('Okay, that\'s my online moniker. You may pass.')
+			
+		}else{
+				//INCORRECT!
+			alert('Nope. My name is Dustin Mundy! I can be found around the web using the moniker VergilPrime.')
+			
+		}
+		
+	}
+	
+}
+
+	
+	//QUESTION 2
+var devJobGuess = prompt('What was my previous profession?').toLowerCase();
+if(devJobGuess === 'security' || devJobGuess === 'security guard' || devJobGuess === 'guard'){
+		//CORRECT!
+	alert('Yes, I was a security guard before I moved here for about a year and a half.')
+	boolAnswer2 = true
+	
+}else{
+		//INCORRECT!
+	alert('Nope. Nice try.')
+	boolAnswer2 = false
+	
+}
+	
+	
+	//QUESTION 3
+if(devNameGuess === 'dustin' || devNameGuess === 'dustin mundy' ){
+		//CORRECT!
+	alert('')
+	boolAnswer3 = true
+	
+}else{
+		//INCORRECT!
+	alert('')
+	boolAnswer3 = false
+	
+}
+
+	
+	//QUESTION 4
+if(devNameGuess === 'dustin' || devNameGuess === 'dustin mundy' ){
+		//CORRECT!
+	alert('')
+	boolAnswer4 = true
+	
+}else{
+		//INCORRECT!
+	alert('')
+	boolAnswer4 = false
 	
 }
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Updating all files"
+git push origin master
